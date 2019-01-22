d2ed8d3c99973206d818711a1d84952d63e26ecf
Wilson Alexander Almonte Ureña <wilson.almonte@solvex.com.do>
2019-01-22 14:57:48 -0400

    S1L5

---
+++ S1L5/main.js
@@ -0,0 +1,39 @@
+var app =  new Vue({
+    el:'main',
+    data:{
+        mensaje:'Hola a todos, amo Vue js',
+        personas: [
+            {
+                nombre:'Luis Manuel',
+                edad: 20,
+                registrado:false
+            },
+            {
+                nombre:'Mario Martinez',
+                edad: 13,
+                registrado:true
+            },
+            {
+                nombre:'Stanley Lara',
+                edad: 37,
+                registrado:false
+            },
+            {
+                nombre:'Emmanuel Jimenez',
+                edad: 23,
+                registrado:true
+            }
+         ]
+    },
+    computed:{
+        mensajeReverso: function(){
+            return this.mensaje.split('').reverse().join('');
+        },
+        personasMayores: function(){
+            return this.personas.filter(x=>x.edad >= 18);
+        },
+        personasRegistrados: function(){
+            return this.personas.filter(x=>x.registrado);
+        }
+    }
+})
